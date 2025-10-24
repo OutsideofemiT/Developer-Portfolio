a98456b7bb50051098ec017cd3699dd4cf860993
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-10-24 15:58:30 -0500

    added a scroll-top function since skills page link to projects page wasn't starting at the top of the next page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectsPage from "./pages/ProjectsPage";
+
+// Add this ScrollToTop component
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <div className="min-h-screen bg-smoky text-whitesmoke">
       <Router>
+        <ScrollToTop /> {/* Add this line */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<ProjectsPage />} />
@@ -17,5 +29,3 @@ function App() {
 }
 
 export default App;
-
-
